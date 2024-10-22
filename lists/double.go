4b73ca4a55419af18b27f2d143e7610db14bcb9a
Chris Madison <chris_madison@bellsouth.net>
2024-10-22 14:32:40 -0400

    initial DoublyLinkedList implementation

---
+++ lists/double.go
@@ -0,0 +1,203 @@
+package lists
+
+import "strings"
+
+/***************************************************
+ * Node definition and operations
+ ***************************************************/
+type Node struct {
+	Data string
+	Next *Node
+	Prev *Node
+}
+
+func (me *Node) AddAfter(node *Node) {
+	if me.Next != nil {
+		me.Next.Prev = node
+	}
+	node.Next = me.Next
+	node.Prev = me
+	me.Next = node
+}
+
+func (me *Node) DeleteAfter() *Node {
+	if me.Next == nil {
+		panic("Attempt to delete nonexistent node")
+	}
+
+	after := me.Next
+
+	if after.Next != nil {
+		after.Next.Prev = me
+	}
+
+	me.Next = after.Next
+
+	after.Next, after.Prev = nil, nil
+	return after
+}
+
+/***************************************************
+ * DoublyLinkedList definition and operations
+ ***************************************************/
+type DoublyLinkedList struct {
+	top_sentinel    *Node
+	bottom_sentinel *Node
+}
+
+func NewDoublyLinkedList() *DoublyLinkedList {
+	list := &DoublyLinkedList{&Node{}, &Node{}}
+	list.top_sentinel.Next = list.bottom_sentinel
+	list.bottom_sentinel.Prev = list.top_sentinel
+	return list
+}
+
+func (l *DoublyLinkedList) Add(value string) {
+	l.top_sentinel.AddAfter(&Node{value, nil, nil})
+}
+
+func (l *DoublyLinkedList) AddList(list DoublyLinkedList) {
+	if list.Length() == 0 {
+		return
+	}
+
+	lastNode := l.LastNode()
+
+	for node := list.top_sentinel.Next; node != list.bottom_sentinel; node = node.Next {
+		lastNode.AddAfter(&Node{node.Data, nil, nil})
+		lastNode = lastNode.Next
+	}
+}
+
+func (l *DoublyLinkedList) AddRange(values []string) {
+	lastNode := l.LastNode()
+
+	for _, value := range values {
+		lastNode.AddAfter(&Node{value, nil, nil})
+		lastNode = lastNode.Next
+	}
+}
+
+func (l *DoublyLinkedList) Append(value string) {
+	lastNode := l.LastNode()
+	lastNode.AddAfter(&Node{value, nil, nil})
+}
+
+func (l *DoublyLinkedList) Clear() {
+	l.top_sentinel.Next = l.bottom_sentinel
+	l.top_sentinel.Prev = nil
+	l.bottom_sentinel.Prev = l.top_sentinel
+	l.bottom_sentinel.Next = nil
+}
+
+func (l *DoublyLinkedList) Clone() *DoublyLinkedList {
+	clone := NewDoublyLinkedList()
+	clone.AddList(*l)
+	return clone
+}
+
+func (l *DoublyLinkedList) Contains(value string) bool {
+	node := l.Find(value)
+	return node != nil
+}
+
+func (l *DoublyLinkedList) Find(value string) *Node {
+	for node := l.top_sentinel.Next; node != nil; node = node.Next {
+		if node.Data == value {
+			return node
+		}
+	}
+	return nil
+}
+
+func (l *DoublyLinkedList) IsEmpty() bool {
+	if l.top_sentinel.Next != l.bottom_sentinel {
+		return false
+	}
+	return true
+}
+
+func (l *DoublyLinkedList) LastNode() *Node {
+	return l.bottom_sentinel.Prev
+}
+
+func (l *DoublyLinkedList) Length() int {
+	//likely better to keep a running count
+	//TODO: implement a counter in the DoublyLinkedList struct
+	var length int
+	for node := l.top_sentinel.Next; node != l.bottom_sentinel; node = node.Next {
+		length++
+	}
+	return length
+}
+
+func (l *DoublyLinkedList) Remove(value string) *Node {
+	var prev *Node
+	for node := l.top_sentinel; node.Next != l.bottom_sentinel; node = node.Next {
+		if node.Next.Data == value {
+			prev = node
+			break
+		}
+	}
+	if prev != nil {
+		return prev.DeleteAfter()
+	}
+	return nil
+}
+
+func (l *DoublyLinkedList) RemoveAt(index int) *Node {
+	var prev *Node
+	for i, node := 0, l.top_sentinel; node.Next != l.bottom_sentinel; i, node = i+1, node.Next {
+		if i == index {
+			prev = node
+			break
+		}
+	}
+	if prev != nil {
+		return prev.DeleteAfter()
+	}
+	return nil
+}
+
+func (l *DoublyLinkedList) ToSlice() []Node {
+	var slice []Node
+
+	for node := l.top_sentinel.Next; node != l.bottom_sentinel; node = node.Next {
+		slice = append(slice, *node)
+	}
+	return slice
+}
+
+func (l *DoublyLinkedList) ToString(separator string) string {
+	var builder strings.Builder
+
+	for node := l.top_sentinel.Next; node != l.bottom_sentinel; node = node.Next {
+		if node != l.top_sentinel.Next {
+			builder.WriteString(separator)
+		}
+		builder.WriteString(node.Data)
+	}
+	return builder.String()
+}
+
+func (l *DoublyLinkedList) ToStringMax(separator string, max int) string {
+	var builder strings.Builder
+
+	i := 0
+	for node := l.top_sentinel.Next; node != l.bottom_sentinel && i < max; node = node.Next {
+		if i > 0 {
+			builder.WriteString(separator)
+		}
+		builder.WriteString(node.Data)
+		i++
+	}
+	return builder.String()
+}
+
+func (l *DoublyLinkedList) Values() []string {
+	var values []string
+	for node := l.top_sentinel.Next; node != l.bottom_sentinel; node = node.Next {
+		values = append(values, node.Data)
+	}
+	return values
+}
---
+++ lists/double_test.go
@@ -0,0 +1,516 @@
+package lists
+
+import (
+	"testing"
+)
+
+func TestNode_AddAfter(t *testing.T) {
+	t.Parallel()
+	aNode := Node{"Apple", nil, nil}
+	bNode := Node{"Banana", nil, nil}
+
+	aNode.AddAfter(&bNode)
+
+	if aNode.Next != &bNode {
+		t.Errorf("aNode.Next = %v, want %v", aNode.Next, &bNode)
+	}
+
+	if bNode.Prev != &aNode {
+		t.Errorf("bNode.Prev = %v, want %v", bNode.Prev, &aNode)
+	}
+}
+
+func TestNode_DeleteAfter(t *testing.T) {
+	t.Parallel()
+	aNode := Node{"Apple", nil, nil}
+	bNode := Node{"Banana", nil, nil}
+
+	aNode.AddAfter(&bNode)
+	deleted := aNode.DeleteAfter()
+
+	if deleted != &bNode {
+		t.Errorf("deleted = %v, want %v", deleted, &bNode)
+	}
+	if aNode.Next != nil {
+		t.Errorf("aNode.Next = %v, want nil", aNode.Next)
+	}
+}
+
+func TestNode_DeleteAfter_Panic(t *testing.T) {
+	t.Parallel()
+	aNode := Node{"Apple", nil, nil}
+
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("DeleteAfter did not panic")
+		}
+	}()
+
+	aNode.DeleteAfter()
+}
+
+func TestDoublyLinkedList_NewDoublyLinkedList(t *testing.T) {
+	t.Parallel()
+	list := NewDoublyLinkedList()
+
+	if list.top_sentinel.Next != list.bottom_sentinel {
+		t.Errorf("list.top_sentinel.Next = %v, want %v", list.top_sentinel.Next, list.bottom_sentinel)
+	}
+
+	if list.top_sentinel.Prev != nil {
+		t.Errorf("list.top_sentinel.Prev = %v, want nil", list.top_sentinel.Prev)
+	}
+
+	if list.bottom_sentinel.Prev != list.top_sentinel {
+		t.Errorf("list.bottom_sentinel.Prev = %v, want %v", list.bottom_sentinel.Prev, list.top_sentinel)
+	}
+
+	if list.bottom_sentinel.Next != nil {
+		t.Errorf("list.bottom_sentinel.Next = %v, want nil", list.bottom_sentinel.Next)
+	}
+}
+
+func TestDoublyLinkedList_AddList(t *testing.T) {
+	t.Parallel()
+	list := NewDoublyLinkedList()
+	other := NewDoublyLinkedList()
+	values := []string{"Apple", "Banana", "Cherry"}
+
+	list.AddRange(values)
+	other.AddRange(values)
+
+	list.AddList(*other)
+
+	lastNode := list.LastNode()
+
+	if lastNode.Data != "Cherry" {
+		t.Errorf("lastNode.Data = %q, want %q", lastNode.Data, "Date")
+	}
+}
+
+func TestDoublyLinkedList_AddList_Empty(t *testing.T) {
+	t.Parallel()
+	list := NewDoublyLinkedList()
+	other := NewDoublyLinkedList()
+
+	list.AddList(*other)
+
+	if list.top_sentinel.Next != list.bottom_sentinel {
+		t.Errorf("list.sentinel.Next = %v, want  bottom_sentinal: %v", list.top_sentinel.Next, list.bottom_sentinel)
+	}
+}
+
+func TestDoublyLinkedList_AddList_WithValues(t *testing.T) {
+	t.Parallel()
+	list := NewDoublyLinkedList()
+	other := NewDoublyLinkedList()
+	values := []string{"Apple", "Banana", "Cherry"}
+
+	list.Add("Date")
+	other.AddRange(values)
+
+	list.AddList(*other)
+
+	lastNode := list.LastNode()
+
+	if lastNode.Data != "Cherry" {
+		t.Errorf("lastNode.Data = %q, want %q", lastNode.Data, "Date")
+	}
+
+	if list.top_sentinel.Next.Data != "Date" {
+		t.Errorf("list.sentinel.Next.Data = %q, want %q", list.top_sentinel.Next.Data, "Date")
+	}
+
+	if list.Length() != 4 {
+		t.Errorf("list.Length() = %d, want 4", list.Length())
+	}
+}
+
+func TestDoublyLinkedList_Add(t *testing.T) {
+	t.Parallel()
+	list := NewDoublyLinkedList()
+
+	list.Add("Apple")
+
+	if list.top_sentinel.Next.Data != "Apple" {
+		t.Errorf("list.top_sentinel.Next.Data = %s, want Apple", list.top_sentinel.Next.Data)
+	}
+
+	if list.bottom_sentinel.Prev.Data != "Apple" {
+		t.Errorf("list.bottom_sentinel.Prev.Data = %s, want Apple", list.bottom_sentinel.Prev.Data)
+	}
+
+	if list.top_sentinel.Next.Next != list.bottom_sentinel {
+		t.Errorf("list.top_sentinel.Next.Next = %v, want %v", list.top_sentinel.Next.Next, list.bottom_sentinel)
+	}
+
+	if list.bottom_sentinel.Prev.Prev != list.top_sentinel {
+		t.Errorf("list.bottom_sentinel.Prev.Prev = %v, want %v", list.bottom_sentinel.Prev.Prev, list.top_sentinel)
+	}
+}
+
+func TestDoublyLinkedList_AddRange(t *testing.T) {
+	t.Parallel()
+	list := NewDoublyLinkedList()
+
+	values := []string{"Apple", "Banana", "Cherry"}
+
+	list.AddRange(values)
+
+	node := list.top_sentinel.Next
+	for i, value := range values {
+		if node == nil {
+			t.Errorf("node = nil, want &Node{%q, nil}", value)
+		} else if node.Data != value {
+			t.Errorf("node.Data = %q, want %q", node.Data, value)
+		}
+		if i < len(values)-1 {
+			node = node.Next
+		}
+	}
+}
+
+func TestDoublyLinkedList_Append(t *testing.T) {
+	t.Parallel()
+	list := NewDoublyLinkedList()
+	values := []string{"Apple", "Banana", "Cherry"}
+
+	list.AddRange(values)
+	list.Append("Date")
+
+	lastNode := list.LastNode()
+
+	if lastNode.Data != "Date" {
+		t.Errorf("lastNode.Data = %q, want %q", lastNode.Data, "Date")
+	}
+}
+
+func TestDoublyLinkedList_Clone(t *testing.T) {
+	t.Parallel()
+	list := NewDoublyLinkedList()
+	values := []string{"Apple", "Banana", "Cherry"}
+
+	list.AddRange(values)
+
+	clone := list.Clone()
+
+	if clone == list {
+		t.Errorf("clone = list, want different instances")
+	}
+
+	if clone.ToString(", ") != list.ToString(", ") {
+		t.Errorf("clone.ToString() = %q, want %q", clone.ToString(", "), list.ToString(", "))
+	}
+}
+
+func TestDoublyLinkedList_ToStringMax(t *testing.T) {
+	t.Parallel()
+	list := NewDoublyLinkedList()
+	values := []string{"Apple", "Banana", "Cherry"}
+
+	list.AddRange(values)
+
+	got := list.ToStringMax(", ", 2)
+	want := "Apple, Banana"
+
+	if got != want {
+		t.Errorf("got = %q, want %q", got, want)
+	}
+}
+
+func TestDoublyLinkedList_Values(t *testing.T) {
+	t.Parallel()
+	list := NewDoublyLinkedList()
+	values := []string{"Apple", "Banana", "Cherry"}
+
+	list.AddRange(values)
+
+	got := list.Values()
+	for i, value := range values {
+		if i < len(got) {
+			if got[i] != value {
+				t.Errorf("got[%d] = %q, want %q", i, got[i], value)
+			}
+		} else {
+			t.Errorf("got[%d] = _, want %q", i, value)
+		}
+	}
+}
+
+func TestDoublyLinkedList_toString(t *testing.T) {
+	t.Parallel()
+	list := NewDoublyLinkedList()
+	values := []string{"Apple", "Banana", "Cherry"}
+
+	list.AddRange(values)
+
+	got := list.ToString(", ")
+	want := "Apple, Banana, Cherry"
+
+	if got != want {
+		t.Errorf("got = %q, want %q", got, want)
+	}
+}
+
+func TestDoublyLinkedList_Length_NotEmpty(t *testing.T) {
+	t.Parallel()
+	list := NewDoublyLinkedList()
+	values := []string{"Apple", "Banana", "Cherry"}
+
+	list.AddRange(values)
+
+	got := list.Length()
+	want := len(values)
+
+	if got != want {
+		t.Errorf("got = %d, want %d", got, want)
+	}
+}
+
+func TestDoublyLinkedList_Length_Empty(t *testing.T) {
+	t.Parallel()
+	list := NewDoublyLinkedList()
+
+	got := list.Length()
+	want := 0
+
+	if got != want {
+		t.Errorf("got = %d, want %d", got, want)
+	}
+}
+
+func TestDoublyLinkedList_isEmpty_Empty(t *testing.T) {
+	t.Parallel()
+	list := NewDoublyLinkedList()
+
+	if !list.IsEmpty() {
+		t.Errorf("list.isEmpty() = false, want true")
+	}
+}
+
+func TestDoublyLinkedList_isEmpty_NotEmpty(t *testing.T) {
+	t.Parallel()
+	list := NewDoublyLinkedList()
+	values := []string{"Apple", "Banana", "Cherry"}
+
+	list.AddRange(values)
+
+	if list.IsEmpty() {
+		t.Errorf("list.isEmpty() = true, want false")
+	}
+}
+
+func TestDoublyLinkedList_Find(t *testing.T) {
+	t.Parallel()
+	list := NewDoublyLinkedList()
+	values := []string{"Apple", "Banana", "Cherry"}
+
+	list.AddRange(values)
+
+	for _, value := range values {
+		node := list.Find(value)
+		if node == nil {
+			t.Errorf("node = nil, want &Node{%q, nil}", value)
+		} else if node.Data != value {
+			t.Errorf("node.Data = %q, want %q", node.Data, value)
+		}
+	}
+}
+
+func TestDoublyLinkedList_Find_NotFound(t *testing.T) {
+	t.Parallel()
+	list := NewDoublyLinkedList()
+	values := []string{"Apple", "Banana", "Cherry"}
+
+	list.AddRange(values)
+
+	node := list.Find("Date")
+
+	if node != nil {
+		t.Errorf("node = %v, want nil", node)
+	}
+}
+
+func TestDoublyLinkedList_Contains(t *testing.T) {
+	t.Parallel()
+	list := NewDoublyLinkedList()
+	values := []string{"Apple", "Banana", "Cherry"}
+
+	list.AddRange(values)
+
+	for _, value := range values {
+		if !list.Contains(value) {
+			t.Errorf("list.Contains(%q) = false, want true", value)
+		}
+	}
+}
+
+func TestDoublyLinkedList_Contains_NotFound(t *testing.T) {
+	t.Parallel()
+	list := NewDoublyLinkedList()
+	values := []string{"Apple", "Banana", "Cherry"}
+
+	list.AddRange(values)
+
+	if list.Contains("Date") {
+		t.Errorf("list.Contains(%q) = true, want false", "Date")
+	}
+}
+
+func TestDoublyLinkedList_Remove(t *testing.T) {
+	t.Parallel()
+	list := NewDoublyLinkedList()
+	values := []string{"Apple", "Banana", "Cherry"}
+
+	list.AddRange(values)
+
+	for _, value := range values {
+		removed := list.Remove(value)
+		if removed == nil {
+			t.Errorf("list.Remove(%q) = false, want true", value)
+		}
+	}
+}
+
+func TestDoublyLinkedList_Remove_NotFound(t *testing.T) {
+	t.Parallel()
+	list := NewDoublyLinkedList()
+	values := []string{"Apple", "Banana", "Cherry"}
+
+	list.AddRange(values)
+
+	removed := list.Remove("Date")
+
+	if removed != nil {
+		t.Errorf("list.Remove(%q) = true, want false", "Date")
+	}
+}
+
+func TestDoublyLinkedList_RemoveAt(t *testing.T) {
+	t.Parallel()
+	list := NewDoublyLinkedList()
+	values := []string{"Apple", "Banana", "Cherry"}
+
+	list.AddRange(values)
+
+	for i := 0; i < len(values); i++ {
+		removed := list.RemoveAt(0)
+		if removed == nil {
+			t.Errorf("list.RemoveAt(%d) = false, want true", i)
+		}
+	}
+
+	if list.top_sentinel.Next != list.bottom_sentinel {
+		t.Errorf("list.sentinel.Next = %v, want bottom_sentinel", list.top_sentinel.Next)
+	}
+
+	if list.Length() != 0 {
+		t.Errorf("list.Length() = %d, want 0", list.Length())
+	}
+
+}
+
+func TestDoublyLinkedList_RemoveAt_OutOfRange(t *testing.T) {
+	t.Parallel()
+	list := NewDoublyLinkedList()
+	values := []string{"Apple", "Banana", "Cherry"}
+
+	list.AddRange(values)
+
+	removed := list.RemoveAt(8)
+
+	if removed != nil {
+		t.Errorf("list.RemoveAt(8) = true, want false")
+	}
+}
+
+func TestDoublyLinkedList_LastNode(t *testing.T) {
+	t.Parallel()
+	list := NewDoublyLinkedList()
+	values := []string{"Apple", "Banana", "Cherry"}
+
+	list.AddRange(values)
+
+	lastNode := list.LastNode()
+
+	if lastNode.Data != "Cherry" {
+		t.Errorf("lastNode.Data = %q, want %q", lastNode.Data, "Date")
+	}
+}
+
+func TestDoublyLinkedList_Clear(t *testing.T) {
+	t.Parallel()
+	list := NewDoublyLinkedList()
+	values := []string{"Apple", "Banana", "Cherry"}
+
+	list.AddRange(values)
+
+	list.Clear()
+
+	if list.top_sentinel.Next != list.bottom_sentinel {
+		t.Errorf("list.sentinel.Next = %v, want bottom_sentinel", list.top_sentinel.Next)
+	}
+}
+
+func TestDoublyLinkedList_Clear_Empty(t *testing.T) {
+	t.Parallel()
+	list := NewDoublyLinkedList()
+
+	list.Clear()
+
+	if list.top_sentinel.Next != list.bottom_sentinel {
+		t.Errorf("list.sentinel.Next = %v, want bottom_sentinel", list.top_sentinel.Next)
+	}
+}
+
+func TestDoublyLinkedList_ToSlice(t *testing.T) {
+	t.Parallel()
+	list := NewDoublyLinkedList()
+	values := []string{"Apple", "Banana", "Cherry"}
+
+	list.AddRange(values)
+
+	got := list.ToSlice()
+	for i, value := range values {
+		if i < len(got) {
+			if got[i].Data != value {
+				t.Errorf("got[%d] = %q, want %q", i, got[i].Data, value)
+			}
+		} else {
+			t.Errorf("got[%d] = _, want %q", i, value)
+		}
+	}
+}
+
+func TestDoublyLinkedList_ToSlice_Empty(t *testing.T) {
+	t.Parallel()
+	list := NewDoublyLinkedList()
+
+	got := list.ToSlice()
+
+	if len(got) != 0 {
+		t.Errorf("len(got) = %d, want 0", len(got))
+	}
+}
+
+func TestDoublyLinkedList_ToSlice_WithValues(t *testing.T) {
+	t.Parallel()
+	list := NewDoublyLinkedList()
+	values := []string{"Apple", "Banana", "Cherry"}
+
+	list.AddRange(values)
+	list.Append("Date")
+
+	values = append(values, "Date")
+
+	got := list.ToSlice()
+	for i, value := range values {
+		if i < len(got) {
+			if got[i].Data != value {
+				t.Errorf("got[%d] = %q, want %q", i, got[i].Data, value)
+			}
+		} else {
+			t.Errorf("got[%d] = _, want %q", i, value)
+		}
+	}
+}
